156fa58de42b4dfae3784034bfcb470e7316e51a
JorgeECampos <jorge9campos77@gmail.com>
2025-12-08 14:00:53 -0600

    Test-questions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import path from 'node:path';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   adapter: vercel(),
   output: 'server',
-  integrations: [react(), tailwind()],
-  site: 'https://astro-portfolio.vercel.app',
+  site: 'https://jorgecamposdocs.vercel.app',
+  integrations: [react(), tailwind(), sitemap()],
   vite: {
     resolve: {
       alias: {
